59be8211a4c84891446ee3103d1ac8fbc7997ae7
Shirish Koirala <shirishkoirala@gmail.com>
2023-06-05 07:02:04 +1000

    Initial Commit

---
+++ src/main/java/com/shirishkoirala/fontawesome/IconDrawable.kt
@@ -0,0 +1,66 @@
+package com.shirishkoirala.fontawesome
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.drawable.ShapeDrawable
+
+
+class IconDrawable(context: Context, string: String) : ShapeDrawable() {
+    private var textPaint: Paint = Paint()
+    private var text: String = string
+    private var height = 10
+    private var width = 10
+    private var fontSize = 8
+    private var context: Context
+
+    init {
+        this.context = context
+        textPaint.isAntiAlias = true
+        textPaint.isFakeBoldText = false
+        textPaint.style = Paint.Style.FILL
+        textPaint.typeface = Typeface.createFromAsset(context.assets, "font/solid.otf")
+        textPaint.textAlign = Paint.Align.CENTER
+        textPaint.color = Color.WHITE
+        paint.color = Color.TRANSPARENT
+    }
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        val r = bounds
+
+        val count = canvas.save()
+        canvas.translate(r.left.toFloat(), r.top.toFloat())
+
+        val width = if (width < 0) r.width() else width
+        val height = if (height < 0) r.height() else height
+        val fontSize = if (fontSize < 0) Math.min(width, height) / 2 else fontSize
+        textPaint.textSize = fontSize.toFloat()
+        canvas.drawText(
+            text, (width / 2).toFloat(),
+            height / 2 - (textPaint.descent() + textPaint.ascent()) / 2, textPaint
+        )
+        canvas.restoreToCount(count)
+    }
+
+    override fun setAlpha(alpha: Int) {
+        textPaint.alpha = alpha
+    }
+
+    override fun setColorFilter(cf: ColorFilter?) {
+        textPaint.colorFilter = cf
+    }
+
+    override fun getIntrinsicWidth(): Int {
+        return width
+    }
+
+    override fun getIntrinsicHeight(): Int {
+        return height
+    }
+
+
+}
---
+++ src/main/java/com/shirishkoirala/fontawesome/IconView.kt
@@ -0,0 +1,45 @@
+package com.shirishkoirala.fontawesome
+
+import android.content.Context
+import android.util.AttributeSet
+
+class IconView(context: Context) :
+    androidx.appcompat.widget.AppCompatImageView(context) {
+    constructor(context: Context, attrs: AttributeSet?) : this(context) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : this(context, attrs) {
+        init(context, attrs)
+    }
+
+    fun init(context: Context, attributeSet: AttributeSet?) {
+//        val attr =
+//            context.obtainStyledAttributes(attributeSet, R.styleable.FloatingActionButton, 0, 0)
+//        mColorNormal = attr.getColor(
+//            R.styleable.FloatingActionButton_fab_colorNormal,
+//            getColor(R.color.holo_blue_dark)
+//        )
+//        mColorPressed = attr.getColor(
+//            R.styleable.FloatingActionButton_fab_colorPressed,
+//            getColor(R.color.holo_blue_light)
+//        )
+//        mColorDisabled = attr.getColor(
+//            R.styleable.FloatingActionButton_fab_colorDisabled,
+//            getColor(R.color.darker_gray)
+//        )
+//        mSize = attr.getInt(
+//            R.styleable.FloatingActionButton_fab_size,
+//            com.talk5.audio.views.floatingactionbutton.FloatingActionButton.SIZE_NORMAL
+//        )
+//        mIcon = attr.getResourceId(R.styleable.FloatingActionButton_fab_icon, 0)
+//        mTitle = attr.getString(R.styleable.FloatingActionButton_fab_title)
+//        mStrokeVisible = attr.getBoolean(R.styleable.FloatingActionButton_fab_stroke_visible, true)
+//        attr.recycle()
+//        updateCircleSize()
+//        mShadowRadius = getDimension(R.dimen.fab_shadow_radius)
+//        mShadowOffset = getDimension(R.dimen.fab_shadow_offset)
+//        updateDrawableSize()
+//        updateBackground()
+    }
+}
